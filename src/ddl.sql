fd4631d971d1cc37da27a3112ebf7505ca3c406b
amanojs <takashivue07@gmail.com>
2020-09-21 16:15:59 +0900

    データの可視化pdf ver 1.0.0

--- src/ddl.sql
+++ src/ddl.sql
@@ -53,6 +53,45 @@ insert into seat values(1,"B7","test@test.com",1,"2019-11-08 00:00:00");
 insert into seat values(1,"B8","test@test.com",0,"2019-11-08 00:00:00");
 insert into seat values(1,"B9","test@test.com",0,"2019-11-08 00:00:00");
 
+insert into seat values(999,"A1","test@test.com",0,"2019-10-08 00:00:00");
+insert into seat values(999,"A2","test@test.com",1,"2019-10-08 00:00:00");
+insert into seat values(999,"A3","test@test.com",2,"2019-10-08 00:00:00");
+insert into seat values(999,"A4","test@test.com",1,"2019-10-08 00:00:00");
+insert into seat values(999,"A5","test@test.com",0,"2019-10-08 00:00:00");
+insert into seat values(999,"A6","test@test.com",0,"2019-10-08 00:00:00");
+insert into seat values(999,"A7","test@test.com",1,"2019-10-08 00:00:00");
+insert into seat values(999,"A8","test@test.com",0,"2019-10-08 00:00:00");
+insert into seat values(999,"A9","test@test.com",0,"2019-10-08 00:00:00");
+insert into seat values(999,"B1","test@test.com",0,"2019-10-08 00:00:00");
+insert into seat values(999,"B2","test@test.com",1,"2019-10-08 00:00:00");
+insert into seat values(999,"B3","test@test.com",2,"2019-10-08 00:00:00");
+insert into seat values(999,"B4","test@test.com",1,"2019-10-08 00:00:00");
+insert into seat values(999,"B5","test@test.com",0,"2019-10-08 00:00:00");
+insert into seat values(999,"B6","test@test.com",0,"2019-10-08 00:00:00");
+insert into seat values(999,"B7","test@test.com",1,"2019-10-08 00:00:00");
+insert into seat values(999,"B8","test@test.com",0,"2019-10-08 00:00:00");
+insert into seat values(999,"B9","test@test.com",0,"2019-10-08 00:00:00");
+
+
+insert into seat values(998,"A1","test@test.com",0,"2020-01-08 00:00:00");
+insert into seat values(998,"A2","test@test.com",0,"2020-01-08 00:00:00");
+insert into seat values(998,"A3","test@test.com",0,"2020-01-08 00:00:00");
+insert into seat values(998,"A4","test@test.com",0,"2020-01-08 00:00:00");
+insert into seat values(998,"A5","test@test.com",0,"2020-01-08 00:00:00");
+insert into seat values(998,"A6","test@test.com",0,"2020-01-08 00:00:00");
+insert into seat values(998,"A7","test@test.com",0,"2020-01-08 00:00:00");
+insert into seat values(998,"A8","test@test.com",0,"2020-01-08 00:00:00");
+insert into seat values(998,"A9","test@test.com",0,"2020-01-08 00:00:00");
+insert into seat values(998,"B1","test@test.com",0,"2020-01-08 00:00:00");
+insert into seat values(998,"B2","test@test.com",0,"2020-01-08 00:00:00");
+insert into seat values(998,"B3","test@test.com",0,"2020-01-08 00:00:00");
+insert into seat values(998,"B4","test@test.com",0,"2020-01-08 00:00:00");
+insert into seat values(998,"B5","test@test.com",0,"2020-01-08 00:00:00");
+insert into seat values(998,"B6","test@test.com",0,"2020-01-08 00:00:00");
+insert into seat values(998,"B7","test@test.com",0,"2020-01-08 00:00:00");
+insert into seat values(998,"B8","test@test.com",0,"2020-01-08 00:00:00");
+insert into seat values(998,"B9","test@test.com",0,"2020-01-08 00:00:00");
+
 create table kind(
   kind int(1) not null,
   `name` varchar(20) not null,
